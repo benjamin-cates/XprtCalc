01bdecc808f10611c418a2eda1cac3da3509ff0a
Benjamin Cates <bjc2dogs@gmail.com>
2022-07-19 11:41:55 -0700

    Web - add runline

--- wasm/wasm.cpp
+++ wasm/wasm.cpp
@@ -11,11 +11,20 @@ namespace wasm {
         catch(const char* mess) { return "Error:" + string(mess); }
         catch(...) { return "Error: unknown"; }
     }
+    string runLine(string call) {
+        try {
+            return Program::runLine(call).getStr();
+        }
+        catch(string mess) { return "Error: " + mess; }
+        catch(const char* mess) { return "Error:" + string(mess); }
+        catch(...) { return "Error: unknown"; }
+    }
 };
 
 using namespace emscripten;
 EMSCRIPTEN_BINDINGS(module) {
     function("evaluate", &wasm::evaluate);
+    function("runLine", &wasm::runLine);
 }
 
 int main() {
