4f134ebf736e4b46c68f2d7ee2bd97aee16df392
Benjamin Cates <bjc2dogs@gmail.com>
2022-07-21 19:27:28 -0700

    Basic web functionality

--- wasm/driver.js
+++ wasm/driver.js
@@ -5,5 +5,48 @@ function evaluate(xpr) {
     catch(e) {
         console.error(e);
     }
+}
+function run_line(str, coloredInput = "") {
+    if(coloredInput == "") coloredInput = Module.highlightExpression(str);
+    let out = document.createElement("div");
+    out.className = "output_element";
+    out.innerHTML = coloredInput + "<br>" + Module.runLineWithColor(str);
+    document.querySelector("#input_element").value = "";
+    document.querySelector("#highlight_output").innerHTML = "";
+    document.querySelector("#output").prepend(out);
+}
 
+function update_highlight(str) {
+    this.textArea = document.querySelector("#input_element");
+    this.output = document.querySelector("#highlight_output");
+    if(str[str.length - 1] == '\n') {
+        if(str.length == 1)
+            this.textArea.value = "";
+        else run_line(str.substring(0, str.length - 1), this.output.innerHTML);
+        return;
+    }
+    this.text = str;
+    this.textArea.style.height = "1px";
+    document.querySelector("#input").style.height = (this.textArea.scrollHeight) + "px";
+    this.textArea.style.height = (this.textArea.scrollHeight) + "px";
+    this.output.innerHTML = Module.highlightLine(this.text);
+}
+window.onload = _ => {
+    resize();
+    window.addEventListener("resize", resize);
+    update_highlight("");
+}
+
+function resize() {
+    let ioWidth = document.querySelector("#io").clientWidth;
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    if(ioWidth < 700) {
+        document.querySelector("#input").style.width = "100%";
+        document.querySelector("#output").style.width = "100%";
+    }
+    else {
+        document.querySelector("#input").style.width = "50%";
+        document.querySelector("#output").style.width = "50%";
+    }
 }
--- wasm/wasm.cpp
+++ wasm/wasm.cpp
@@ -11,20 +11,54 @@ namespace wasm {
         catch(const char* mess) { return "Error:" + string(mess); }
         catch(...) { return "Error: unknown"; }
     }
-    string runLine(string call) {
-        try {
-            return Program::runLine(call).getStr();
+    string highlight(string str, string colors) {
+        if(str == "") return "";
+        string out = "";
+        char prevColor = 0;
+        for(int i = 0;i < colors.length();i++) {
+            if(colors[i] != prevColor || i == 0) {
+                if(i != 0) out += "</span>";
+                out += "<span class='COL";
+                out += colors[i];
+                out += "'>";
+                prevColor = colors[i];
+            }
+            if(str[i] == '&') out += "&amp;";
+            else if(str[i] == '<') out += "&lt;";
+            else if(str[i] == '>') out += "&gt;";
+            else if(str[i] == '"') out += "&quot;";
+            else if(str[i] == ' ') out += "&#32;";
+            else out += str[i];
         }
-        catch(string mess) { return "Error: " + mess; }
-        catch(const char* mess) { return "Error:" + string(mess); }
-        catch(...) { return "Error: unknown"; }
+        out += "</span>";
+        return out;
+    }
+    string highlightLine(string str) {
+        string colors = Expression::colorLine(str, Program::parseCtx);
+        return highlight(str, colors);
+    }
+    string highlightExpression(string str) {
+        string colors(str.length(),Expression::hl_error);
+        Expression::color(str,colors.begin(),Program::parseCtx);
+        return highlight(str,colors);
+    }
+    string runLine(string call) {
+        return Program::runLine(call).getStr();
+    }
+    string runLineWithColor(string call) {
+        ColoredString res = Program::runLine(call);
+        return highlight(res.getStr(), res.getColor());
     }
 };
 
 using namespace emscripten;
 EMSCRIPTEN_BINDINGS(module) {
     function("evaluate", &wasm::evaluate);
+    function("highlight", &wasm::highlight);
+    function("highlightLine", &wasm::highlightLine);
+    function("highlightExpression", &wasm::highlightExpression);
     function("runLine", &wasm::runLine);
+    function("runLineWithColor", &wasm::runLineWithColor);
 }
 
 int main() {
